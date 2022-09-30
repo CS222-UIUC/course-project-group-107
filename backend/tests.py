181eeeecf7ad53239da90bf59fe47a05382f46bf
Keshav Trikha <ktrikha2@illinois.edu>
2022-09-30 15:13:21 -0500

    fixing changes and coverage

--- backend/tests.py
+++ backend/tests.py
@@ -1,12 +1,7 @@
 import io
 import sys
 import datapull
-def test_one():
-
-    assert 1 == 1
 
 def test_print():
-    datapull.getdata()
-    assert 2 == 2
-
-test_print()
+    s = datapull.getdata()
+    assert s == ['netid', 'date', 'arccap']
