315f3177b18d37812c4627497dc0a6544840a45e
diegotaveras <diegoat2@illinois.edu>
2022-10-14 21:04:59 -0500

    started models, changed database engine to mysql

--- backend/App/models.py
+++ backend/App/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ARC_Section(models.Model):
+    level_location = models.CharField(max_length=200)
+    capacity =  models.IntegerField(default=0)
+    person_count = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date updated')
