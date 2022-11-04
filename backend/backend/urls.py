32bef96ac509a0f81179b6f3ccc96a29d2c8b01c
diegotaveras <67658114+diegotaveras@users.noreply.github.com>
2022-11-04 15:20:33 -0500

    Revert "prepared env for django development and created App files"

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from accounts import views
-
+from django.urls import path
 
 urlpatterns = [
-    path('App/', include('App.urls')),
-    # path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
-
+    path('admin/', admin.site.urls),
 ]
