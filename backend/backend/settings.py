abcb2b05451b12d3d8942b5ae4a6cb3aae528f85
Keshav Trikha <ktrikha2@illinois.edu>
2022-11-11 15:22:37 -0600

    help

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'cs222'
+        #'TIMEOUT': '180'
     }
 }
 
