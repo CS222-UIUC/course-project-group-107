81802a0d796b1fc01376f82fe928ea06b4814527
sakethboyapally <sakethb2@illinois.edu>
2022-11-21 18:01:33 -0600

    added registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,10 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
 import About from './pages/about';
-import Live from './pages/Live'
+import Live from './pages/Live';
 import Login from './pages/login';
 import Insight from './pages/Insights';
+import Register from './pages/register';
 // import logo from './illini_3_15.png';
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/Live' element={<Live/>} />
           <Route path='/login' element={<Login/>}/>
           <Route path='/insight' element={<Insight/>}/>
+          <Route path='/register' element={<Register/>}/>
         </Routes>
       </Router> 
 );
