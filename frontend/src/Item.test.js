e512e03ce35287a3351d12fbe5925f048ed80d59
sakethboyapally <sakethb2@illinois.edu>
2022-11-30 17:39:17 -0600

    deleted files

--- frontend/src/Item.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Item from './pages/Item';
-
-test('renders equipment list', () => {
-  render(<Item />);
-  const linkElement = screen.getByText(/equipment/i);
-  expect(linkElement).toBeInTheDocument();
-});
