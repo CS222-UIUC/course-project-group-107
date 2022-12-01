6ee5175eac3cd0a32ec59fa8363b1d066c143309
sakethboyapally <sakethb2@illinois.edu>
2022-11-30 18:21:00 -0600

    remove about page

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -16,9 +16,9 @@ const Navbar = () => {
         <NavMenu>
           <NavLink to='/Home'>
             Home  
-          </NavLink>  
-          <NavLink to='/about'>
-            About
+          {/* </NavLink>   */}
+          {/* <NavLink to='/about'>
+            About */}
           </NavLink>
           <NavLink to='/Live'>
             Live Capacities
