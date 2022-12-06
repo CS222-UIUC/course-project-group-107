50589c60b2ec332a115889867ef0d00e20048962
sakethboyapally <sakethb2@illinois.edu>
2022-12-05 23:38:11 -0600

    finished home page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./index.css"
 
 function Home(){
+  const styleObj = {
+    fontSize: 80,
+    color: "#13294B",
+    textAlign: "left",
+    position: "absolute",
+    paddingLeft: "100px",
+}
+const styleObjTwo = {
+  fontSize: 50,
+  color: "#13294B",
+  textAlign: "left",
+  paddingTop: "115px",
+  position: "absolute",
+  paddingLeft: "115px",
+}
     return (
-        <div>
-          Welcome to our web app for information on ARC capacities
+        <div class = "bg-container">
+          <p style={styleObj}>Welcome to the ARC!</p>
+          <p style={styleObjTwo}>Sign in to get started :)</p>
         </div>
       );
 }
