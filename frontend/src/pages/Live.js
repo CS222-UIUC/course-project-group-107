260d04b8adc5355b043da093a570a36b9151e91a
diegotaveras <diegoat2@illinois.edu>
2022-12-07 20:07:32 -0600

    working on test cases

--- frontend/src/pages/Live.js
+++ frontend/src/pages/Live.js
@@ -14,7 +14,7 @@ const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(8),
   textAlign: 'center',
-  fontSize: 24,
+  fontSize: 21,
   color: theme.palette.text.secondary,
   
 }));
@@ -22,54 +22,65 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Live() {
   
-  // const article = { area_name: "MAIN GYM"};
-  //         console.log(article);
-  //           axios.get('http://127.0.0.1:8000/App/api/arcdatacsv/Live/', article, {
-  //             headers: {
-  //               'Content-Type': 'application/json'
-  //             }
-  //           })
-  //             .then(response => console.log(response.data)).catch((error) => console.log( error.response.request._response ) );
+ 
 
   const [Areas, setAreas] = useState([])
-  const [error, setError] = useState(null);
+  
   
   
   useEffect(() => {
   
     axios.get('http://127.0.0.1:8000/App/api/arcdatacsv/').then((response) => {
        setAreas(response.data);
-    }).catch(setError());
+    })
   
 
  }, []);
   
-
+  
   return (
     
     <Box sx={{ width: '100%' }}>
       <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={4}>
-          
-        
-          
         <Item>
-          
           <div>
-            {/* if (Areas[0] != null && Areas.length != 0) */}
-            { Areas[0]?.area_name } : { Areas[0]?.capacity }
+             
+            { Areas[0]?.area_name }: {Areas[0]?.capacity}/60 ({ Math.round(((Areas[0]?.capacity / 60) * 100))})%
+          </div>
+        </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <div>
+          
+          { Areas[1]?.area_name }: {Areas[1]?.capacity}/100 ({ Math.round(((Areas[1]?.capacity / 100) * 100))})%
+          </div>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <div>
+          
+          { Areas[2]?.area_name }: {Areas[2]?.capacity}/50 ({ Math.round(((Areas[2]?.capacity / 50) * 100))})%
+          </div>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <div>
+          
+          { Areas[3]?.area_name }: {Areas[3]?.capacity}/25 ({ Math.round(((Areas[3]?.capacity / 25) * 100))})%
 
           </div>
-          
-          
-        </Item>
-      
+          </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
           <div>
           
-          { Areas[1]?.area_name } : { Areas[1]?.capacity }
+          { Areas[4]?.area_name }: {Areas[4]?.capacity}/20 ({ Math.round(((Areas[4]?.capacity / 20) * 100))})%
+
           </div>
           </Item>
         </Grid>
@@ -77,7 +88,17 @@ export default function Live() {
           <Item>
           <div>
           
-          { Areas[2]?.area_name } : { Areas[2]?.capacity }
+          { Areas[5]?.area_name }: {Areas[5]?.capacity}/15 ({ Math.round(((Areas[5]?.capacity / 15) * 100))})%
+
+          </div>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <div>
+        
+          { Areas[6]?.area_name }: {Areas[6]?.capacity}/50 ({ Math.round(((Areas[6]?.capacity / 50) * 100))})%
+
           </div>
           </Item>
         </Grid>
@@ -85,7 +106,8 @@ export default function Live() {
           <Item>
           <div>
           
-          { Areas[3]?.area_name } : { Areas[3]?.capacity }
+          { Areas[7]?.area_name }: {Areas[7]?.capacity}/50 ({ Math.round(((Areas[7]?.capacity / 50) * 100))})%
+
           </div>
           </Item>
         </Grid>
@@ -93,39 +115,8 @@ export default function Live() {
           <Item>
           <div>
           
-          { Areas[4]?.area_name } : { Areas[4]?.capacity }
-          </div>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <div>
-          
-          { Areas[5]?.area_name } : { Areas[5]?.capacity }
-          </div>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <div>
-          
-          { Areas[6]?.area_name } : { Areas[6]?.capacity }
-          </div>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <div>
-          
-          { Areas[7]?.area_name } : { Areas[7]?.capacity }
-          </div>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <div>
-          
-          { Areas[8]?.area_name } : { Areas[8]?.capacity }
+          { Areas[8]?.area_name }: {Areas[8]?.capacity}/15 ({ Math.round(((Areas[8]?.capacity / 15) * 100))})%
+
           </div>
           </Item>
         </Grid>
